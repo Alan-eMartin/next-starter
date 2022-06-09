381fc72181234e727ed6b573a6aaf577599d654d
Alan <alan.edward.martin@gmail.com>
2022-06-09 19:32:01 -0400

    build: implement storybook, nvmrc

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+import * as NextImage from 'next/image';
+import '!style-loader!css-loader!sass-loader!../src/styles/globals.scss';
+// import '../src/styles/global.scss';
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
